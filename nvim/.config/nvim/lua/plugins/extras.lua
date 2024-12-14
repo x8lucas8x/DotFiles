7eb2162da48dce258a03e34b2bf36aae476b0915
Lucas Lira Gomes <x8lucas8x@gmail.com>
2024-12-14 13:21:46 +0100

    Update neovim to support languages of interest.

---
+++ nvim/.config/nvim/lua/plugins/extras.lua
@@ -0,0 +1,13 @@
+return {
+  -- languages
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  -- dbs
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  -- configs
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+}

c922d2e9f87775d91a16d8a0f7c3e432ee9e4a12
Lucas Lira Gomes <x8lucas8x@gmail.com>
2024-12-31 18:05:10 +0100

    Make stow/neovim use basedpyright for python linting.

--- stow/nvim/.config/nvim/lua/config/options.lua
+++ stow/nvim/.config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_lsp = "basedpyright"
 -- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
 

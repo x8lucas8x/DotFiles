7dea2fedadda360453f9f928f212bfb25b52cf5a
Lucas Lira Gomes <x8lucas8x@gmail.com>
2025-01-02 14:35:48 +0100

    Add aerospace to mac install script and respective config files.

--- install-mac.sh
+++ install-mac.sh
@@ -3,7 +3,8 @@
 brew install fzf zellij neovim starship stow bat lazygit yazi \
   btop htop zsh eza zoxide duf fastfetch sd
 brew install --cask kitty font-fira-code-nerd-font \
-  font-cascadia-code-nf font-cascadia-mono-nf
+  font-cascadia-code-nf font-cascadia-mono-nf \
+  nikitabobko/tap/aerospace
 
 echo "Running stow..."
 cd stow

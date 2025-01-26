8ee10758e5a62cf562a19d23378054691309952c
Lucas Lira Gomes <x8lucas8x@gmail.com>
2025-01-26 19:55:22 +0100

    Update wezterm stow.

--- stow/wezterm/.config/wezterm/wezterm.lua
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local config = wezterm.config_builder()
 
 config.disable_default_key_bindings = true
 config.window_background_opacity = 0.8
-config.window_decorations = "RESIZE"
 config.scrollback_lines = 200000
+config.use_fancy_tab_bar = true
 config.window_frame = {
 	font = wezterm.font({ family = "CaskaydiaCove Nerd Font Mono", weight = "Bold" }),
 	font_size = 14,
@@ -12,5 +12,22 @@ config.window_frame = {
 config.color_scheme = "GruvboxDarkHard"
 config.cursor_blink_rate = 0
 config.default_cursor_style = "SteadyBar"
+
+function make_copy_mouse_binding(streak, button, mods)
+	return {
+		event = { Up = { streak = streak, button = button } },
+		mods = mods,
+		action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor("ClipboardAndPrimarySelection"),
+	}
+end
+
+config.mouse_bindings = {
+	make_copy_mouse_binding(1, "Left", "NONE"),
+	make_copy_mouse_binding(1, "Left", "SHIFT"),
+	make_copy_mouse_binding(1, "Left", "ALT"),
+	make_copy_mouse_binding(1, "Left", "SHIFT|ALT"),
+	make_copy_mouse_binding(2, "Left", "NONE"),
+	make_copy_mouse_binding(3, "Left", "NONE"),
+}
 
 return config

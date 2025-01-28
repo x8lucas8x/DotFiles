889122e5296f8a98e346a3de7213192773f9cd6c
Lucas Lira Gomes <x8lucas8x@gmail.com>
2025-01-28 08:40:16 +0100

    Disable autoformatting on save on nvim stow.

--- stow/nvim/.config/nvim/lua/config/options.lua
+++ stow/nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable autoformatting on save.
+vim.g.autoformat = false
+
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"

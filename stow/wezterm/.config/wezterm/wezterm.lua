70e928f87a9e3e942f574c9d65e6738ea3ab18f6
Lucas Lira Gomes <x8lucas8x@gmail.com>
2025-03-01 18:21:51 +0100

    Add shell integration shortcuts to kitty and wezterm stows.

--- stow/wezterm/.config/wezterm/wezterm.lua
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,6 @@ config.disable_default_key_bindings = true
 config.window_background_opacity = 0.8
 config.scrollback_lines = 200000
 config.use_fancy_tab_bar = true
-
 config.font = wezterm.font("CaskaydiaCove Nerd Font Mono", { weight = "Bold", stretch = "Normal", style = "Normal" })
 config.font_size = 14
 config.color_scheme = "GruvboxDarkHard"
@@ -40,6 +39,11 @@ config.mouse_bindings = {
 	make_copy_mouse_binding(1, "Left", "SHIFT|ALT"),
 	make_copy_mouse_binding(2, "Left", "NONE"),
 	make_copy_mouse_binding(3, "Left", "NONE"),
+  {
+    event = { Down = { streak = 3, button = 'Left' } },
+    action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
+    mods = 'NONE',
+  },
 }
 
 config.keys = {
@@ -47,6 +51,34 @@ config.keys = {
 	{ key = "c", mods = "SUPER", action = copy_clear_or_interrupt_action },
 	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+  { key = 'PageUp', mods = 'CTRL', action = act.ScrollByPage(-1) },
+  { key = 'PageUp', mods = 'CTRL', action = act.ScrollByPage(-1) },
+  { key = 'PageDown', mods = 'SUPER', action = act.ScrollByPage(1) },
+  { key = 'PageDown', mods = 'SUPER', action = act.ScrollByPage(1) },
+  { key = 'Home', mods = 'CTRL', action = act.ScrollToTop },
+  { key = 'Home', mods = 'SUPER', action = act.ScrollToTop },
+  { key = 'End', mods = 'CTRL', action = act.ScrollToBottom },
+  { key = 'End', mods = 'SUPER', action = act.ScrollToBottom },
+  { key = 'UpArrow', mods = 'SHIFT', action = act.ScrollToPrompt(-1) },
+  { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollToPrompt(1) },
+  { key = '+', mods = 'CTRL', action = wezterm.action.IncreaseFontSize },
+  { key = '+', mods = 'SUPER', action = wezterm.action.IncreaseFontSize },
+  { key = '-', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
+  { key = '-', mods = 'SUPER', action = wezterm.action.DecreaseFontSize },
+  { key = '0', mods = 'CTRL', action = wezterm.action.ResetFontSize },
+  { key = '0', mods = 'SUPER', action = wezterm.action.ResetFontSize },
 }
+
+config.unix_domains = {
+  {
+    name = 'unix',
+  },
+}
+
+-- This causes `wezterm` to act as though it was started as
+-- `wezterm connect unix` by default, connecting to the unix
+-- domain on startup.
+-- If you prefer to connect manually, leave out this line.
+config.default_gui_startup_args = { 'connect', 'unix' }
 
 return config
